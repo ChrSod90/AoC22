f49050c9e8c4d33378c539a4d8c34b32e442deed
unknown <chrsod90@gmail.com>
2022-12-03 23:34:29 +0100

    init d2 and d3

---
+++ day2/day2.java
@@ -0,0 +1,16 @@
+package AoC22.day2;
+import java.io.*;
+import java.util.*;
+
+//A X = rock
+//B Y = paper
+//C Z = scissors
+
+public class day2 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader("i:/code/AoC22/day2/input.test"));
+
+
+        br.close();
+    }
+}
---
+++ day3/day3.java
@@ -0,0 +1,12 @@
+package AoC22.day3;
+import java.io.*;
+import java.util.*;
+
+public class day3 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader("i:/code/AoC22/day3/input.test"));
+
+        
+        br.close();
+    }
+}
