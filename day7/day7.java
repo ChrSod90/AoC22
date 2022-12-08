d41a1b91038031d7dc07e2e87b35550b78b48b97
unknown <chrsod90@gmail.com>
2022-12-08 19:54:29 +0100

    day 7 start

--- day7/day7.java
+++ day7/day7.java
@@ -6,7 +6,26 @@ import java.util.*;
 public class day7 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader("i:/code/AoC22/day7/input.test"));
-
+        String line;
+        while ((line = br.readLine()) != null) {
+            String[] commands = line.split(" ");
+            if (commands[0].equals("$")) {
+                if (commands[1].equals("cd")) {
+                    if (commands[2].equals("/")) {
+                        //Do root shit
+                    }else if (commands[2].equals("..")) {
+                        //Get parent
+                    }else{
+                        //enter sub dir
+                    }
+                }
+            }else if (commands[0].equals("dir")) {
+                //create sub dirr to curr dir
+                
+            }else{
+                //add files to curr dirr?
+            }
+        }
         br.close();
     }
 
