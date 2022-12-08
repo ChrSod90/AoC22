41ab670ecd77475e2572b767e2b84467de5c86bd
unknown <chrsod90@gmail.com>
2022-12-08 16:57:44 +0100

    day5 part 1 done, yeah its ugly but I cant be arsed

--- day5/day5.java
+++ day5/day5.java
@@ -6,28 +6,49 @@ import java.util.function.ToIntFunction;
 import java.util.regex.*;
 
 /*
-    [D]    
-[N] [C]    
-[Z] [M] [P]
- 1   2   3 
+    [S] [C]         [Z]            
+[F] [J] [P]         [T]     [N]    
+[G] [H] [G] [Q]     [G]     [D]    
+[V] [V] [D] [G] [F] [D]     [V]    
+[R] [B] [F] [N] [N] [Q] [L] [S]    
+[J] [M] [M] [P] [H] [V] [B] [B] [D]
+[L] [P] [H] [D] [L] [F] [D] [J] [L]
+[D] [T] [V] [M] [J] [N] [F] [M] [G]
+ 1   2   3   4   5   6   7   8   9 
+
  */
 public class day5 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader("i:/code/AoC22/day5/input.test"));
-        Stack stack1 = new Stack<>();
-        Stack stack2 = new Stack<>();
-        Stack stack3 = new Stack<>();
-        stack1.push("Z");
-        stack1.push("N");
-        stack2.push("M");
-        stack2.push("C");
-        stack2.push("D");
-        stack3.push("P");
-        List<Stack<String>> stacks = new ArrayList<>();
-        stacks.add(stack1);
-        stacks.add(stack2);
-        stacks.add(stack3);
+        BufferedReader br = new BufferedReader(new FileReader("i:/code/AoC22/day5/input"));
+        /*
+        String[] boxes = new String[3];
+        boxes[0] = "ZN";
+        boxes[1] = "MCD";
+        boxes[2] = "p";
+        */
+        String[] boxes = new String[9];
+        boxes[0] = "DLJRVGF";
+        boxes[1] = "TPMBVHJS";
+        boxes[2] = "VHMFDGPC";
+        boxes[3] = "MDPNGQ";
+        boxes[4] = "JLHNF";
+        boxes[5] = "NFVQDGTZ";
+        boxes[6] = "FDBL";
+        boxes[7] = "MJBSVDN";
+        boxes[8] = "GLD";
 
+
+        List<Stack<String>> stacks = new ArrayList<>();
+        for (String b : boxes) {
+            Stack stack = new Stack<>();
+            String[] foo = b.split("");
+            for (String b2 : foo) {
+                stack.push(b2);
+            }
+            //System.out.println(stack);
+            stacks.add(stack);
+        }
+        
         String line;
 
         while ((line = br.readLine()) != null) {
@@ -45,12 +66,12 @@ public class day5 {
 
             for (int n = 0; n < amount; n++) {
                 stacks.get(toStack-1).push(stacks.get(fromStack-1).pop());
-                System.out.println("stack " + toStack + stacks.get(toStack-1));
+                //System.out.println("stack " + toStack + stacks.get(toStack-1));
             }
         }
         for (Stack<String> stack : stacks) {
             if (!stack.empty()) {
-                System.out.println(stack.peek());
+                System.out.print(stack.peek());
             }
         }
 
