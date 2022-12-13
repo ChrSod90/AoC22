bf691e6996e30f8e5bae0e24f9165f22acbf0591
unknown <chrsod90@gmail.com>
2022-12-13 01:19:07 +0100

    d9p2 ??

--- day9/day9p2.java
+++ day9/day9p2.java
@@ -4,36 +4,44 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
 
-public class day9 {
+public class day9p2 {
     public static Set<ArrayList<Integer>> visited = new HashSet<>();
 
     public static void main(String[] args) throws Exception {
         String input = Files.readString(Paths.get("i:/code/AOC22/day9/input.test2"));
         String[] lines = input.split("\r\n");
-        ArrayList<Integer> head = new ArrayList<>();
-        ArrayList<Integer> tail = new ArrayList<>();
-        head.add(0);
-        head.add(0);
-        tail.add(0);
-        tail.add(0);
+
+        ArrayList<ArrayList<Integer>> rope = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            rope.add(new ArrayList<>());
+        }
+        for (ArrayList r : rope) {
+            r.add(0);
+            r.add(0);
+        }
 
         for (String l : lines) {
             // System.out.println(l);
             String[] m = l.split(" ");
             // System.out.println(m[1]);
             int nSteps = Integer.parseInt(m[1]);
-            for (int i = 0; i < nSteps; i++) {
-                step(m[0], head);
-                tailFollow(head, tail);
-                //System.out.println("head: " + head.get(0) + " " + head.get(1));
-                //System.out.println("tail: " + tail.get(0) + " " + tail.get(1));
+            boolean last = false;
+            for (int j = 0; j < 9; j++) {
+                last = j==8;
+                for (int i = 0; i < nSteps; i++) {
+                    step(m[0], rope.get(j));
+                    tailFollow(rope.get(j), rope.get(j+1), last);
+                     System.out.println("head: " + rope.get(0) + " " + rope.get(1));
+                    System.out.println("tail: " + rope.get(0) + " " + rope.get(1));
+                }
             }
         }
 
         System.out.println(visited.size());
     }
 
-    private static void tailFollow(ArrayList<Integer> head, ArrayList<Integer> tail) {
+    private static void tailFollow(ArrayList<Integer> head, ArrayList<Integer> tail, boolean last) {
         int diffX = Math.abs(tail.get(0) - head.get(0));
         int diffY = Math.abs(tail.get(1) - head.get(1));
 
@@ -57,8 +65,10 @@ public class day9 {
                 tail.set(0, head.get(0));
             }
         }
-        ArrayList<Integer> temp = new ArrayList<>(tail);
-        visited.add(temp);
+        if (last) {
+            ArrayList<Integer> temp = new ArrayList<>(tail);
+            visited.add(temp);
+        }
     }
 
     private static void step(String m, ArrayList<Integer> rope) {
