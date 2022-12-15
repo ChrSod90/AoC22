532da61565e371622dceaa3775e06f021f021fcf
unknown <chrsod90@gmail.com>
2022-12-15 20:15:52 +0100

    day 10 p1

--- day10/day10.java
+++ day10/day10.java
@@ -1,11 +1,47 @@
 package AoC22.day10;
+
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
 
 public class day10 {
     public static void main(String[] args) throws Exception {
-        String input = Files.readString(Paths.get("i:/code/AOC22/dayx/input.test"));
-        //String[] lines = input.split("\r\n");
+        String input = Files.readString(Paths.get("i:/code/AOC22/day10/input"));
+        String[] lines = input.split("\r\n");
+        int cycleCount = 1;
+        int xReg = 1;
+        int sigStrength = 0;
+        int sigSum = 0;
+        for (String string : lines) {
+            String[] s = string.split(" ");
+            if (s[0].equals("noop")) {
+                cycleCount += 1;
+                sigStrength = xReg * cycleCount;
+                if (cycleCount == 20 || cycleCount == 60 || cycleCount == 100 || cycleCount == 140 || cycleCount == 180
+                        || cycleCount == 220) {
+                    sigSum += sigStrength;
+                }
+            }
+            if (s[0].equals("addx")) {
+                cycleCount += 1;
+                sigStrength = xReg * cycleCount;
+                if (cycleCount == 20 || cycleCount == 60 || cycleCount == 100 || cycleCount == 140 || cycleCount == 180
+                        || cycleCount == 220) {
+                    sigSum += sigStrength;
+                }
+                System.out.println(cycleCount + " : " + xReg + " : " + sigStrength);
+
+                cycleCount += 1;
+                xReg += Integer.parseInt(s[1]);
+                sigStrength = xReg * cycleCount;
+                if (cycleCount == 20 || cycleCount == 60 || cycleCount == 100 || cycleCount == 140 || cycleCount == 180
+                        || cycleCount == 220) {
+                    sigSum += sigStrength;
+                }
+            }
+            System.out.println(cycleCount + " : " + xReg + " : " + sigStrength);
+
+        }
+        System.out.println(sigSum);
     }
 }
