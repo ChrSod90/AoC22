fe8c1fcf537da29fbbaf4d20f523fb1e2dc21220
unknown <chrsod90@gmail.com>
2022-12-15 21:57:16 +0100

    MONKEE! init...

--- day11/day11.java
+++ day11/day11.java
@@ -5,7 +5,10 @@ import java.util.*;
 
 public class day11 {
     public static void main(String[] args) throws Exception {
-        String input = Files.readString(Paths.get("i:/code/AOC22/dayx/input.test"));
-        //String[] lines = input.split("\r\n");
+        String input = Files.readString(Paths.get("i:/code/AOC22/day11/input.test"));
+        String[] monkeys = input.split("\r?\n\r?\n");
+        for (String string : monkeys) {
+            System.out.println("MONK : " + string);
+        }
     }
 }
