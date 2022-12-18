9316a6f7fadcd11b0b35203b79f0682ff336657d
unknown <chrsod90@gmail.com>
2022-12-18 23:41:33 +0100

    day11 progress

--- day11/day11.java
+++ day11/day11.java
@@ -6,9 +6,21 @@ import java.util.*;
 public class day11 {
     public static void main(String[] args) throws Exception {
         String input = Files.readString(Paths.get("i:/code/AOC22/day11/input.test"));
-        String[] monkeys = input.split("\r?\n\r?\n");
-        for (String string : monkeys) {
-            System.out.println("MONK : " + string);
+        String[] monkeysInput = input.split("\r?\n\r?\n");
+        
+        ArrayList<Monkey> monkeys = new ArrayList<>();
+        ArrayList<Integer> divs = new ArrayList<>();
+
+        for (String m : monkeysInput) {
+            String[] lines = m.split("\r?\n");
+            String items = lines[1].substring(18);
+            Monkey monkey = new Monkey(items, lines[2].strip(), String.join("=", lines[3].strip(), lines[4].strip(), lines[5].strip()));
+            monkeys.add(monkey);
+            divs.add(Integer.parseInt(lines[3].strip().split(" ")[3]));
+        }
+        for (Monkey m : monkeys) {
+            System.out.println(m.itemsHeld);
         }
     }
 }
+
